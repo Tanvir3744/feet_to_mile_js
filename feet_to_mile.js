fd500c6d85a59fc0144e2a7c3a83609765bf4dc3
Tanvir3744 <biplobmohammad1@gmail.com>
2022-01-03 21:55:18 +0600

    odd_even_conversion

--- feet_to_mile.js
+++ feet_to_mile.js
@@ -1,7 +1,6 @@
 // fet to mile conversion with javascript
 
 
-
 function feetToMile(feet) {
     if (feet < 0) {
         return "distance cannot be negative"
@@ -12,3 +11,14 @@ function feetToMile(feet) {
 
 let converted = feetToMile(150000);
 console.log(converted);
+
+//simple odd even conversion with javascript
+function oddEven(num) {
+    if (num % 2 === 0) {
+        return "The number is even " + num;
+    } else {
+        return "The number is odd " + num;
+    }
+}
+let even = oddEven(19);
+console.log(even)
